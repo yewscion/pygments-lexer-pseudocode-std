607c42125b2b31cd767389cf51d521c8ea984eab
Christopher Rodriguez <yewscion@gmail.com>
2022-04-03 16:10:19 -0400

    Initial Changes, basics of pseudocode.

--- pygments_lexer_pseudocode/__init__.py
+++ pygments_lexer_pseudocode/__init__.py
@@ -1,22 +1,24 @@
+"""Lexer for (Standardized) Pseudocode in Pygments."""
 import re
 
-from pygments.lexer import RegexLexer, include, bygroups, using, this
-from pygments.token import Error, Punctuation, Text, Comment, Operator, Keyword, Name, String, Number
+from pygments.lexer import RegexLexer, include, bygroups
+from pygments.token import Punctuation, Text, Comment, \
+    Operator, Keyword, Name, String, Number
 
 
 class PseudocodeLexer(RegexLexer):
-    '''
-    A Pseudo code (fr) lexer
-    '''
-    name = 'Pseudocode'
+    """Lex our Pseudocode According to the Standard."""
+
+    name = 'Pseudocode (std)'
     aliases = ['pseudocode', 'pseudo', 'algorithm', 'algo']
     filenames = ['*.algo', '*.pseudocode']
     mimetypes = []
     flags = re.IGNORECASE
 
     def op_replace(lexer, match):
+        """Replace ASCII digraphs with Unicode equivalents."""
         op = match.group(0)
-        
+
         S = ('<=', '>=', '<>', '<-', '^')
         R = ('≤',  '≥',  '≠',  '←',  '↑')
 
@@ -25,57 +27,54 @@ class PseudocodeLexer(RegexLexer):
 
         yield match.start(), Operator, op
 
-    def scomment(lexer, match):
-        s = match.group(1).lower().strip()
-        c = Comment
-
-        directives = ['passage par copie', 'passage par valeur', 'passage par référence', 'passage par reference', 'passage par adresse', 've', 'vs', 've/s']
-
-        if s in directives:
-            c = Comment.Special
-        
-        yield match.start(), c, match.group(0)
-     
     tokens = {
         'root': [
                  (r'\/\*.*\*\/', Comment),
                  (r'(\/\/|#).*\n', Comment),
                  (r'\|', Comment),
-                 (r'\{(.*)\}', scomment),
+                 (r'\{(.*)\}', Comment),
                  include('strings'),
                  include('core'),
+                 (r'(begin +|end +)(.+$)',
+                  bygroups(Keyword, Name.Function)),
                  (r'[a-zéàùçèÉÀÙÇÈ][a-z0-9éàùçèÉÀÙÇÈ_]*', Name.Variable),
                  include('nums'),
                  (r'[\s]+', Text)
         ],
-        'core':[ # Statements
-                 (r'\b(debut|début|fin|si|alors|sinon|fin[_ ]si|tant[ _]que|tantque|fin[ _]tantque|faire|répéter'
-                  r'repeter|type|structure|fin[ _]structure|fonction|procédure|procedure|retourner|renvoyer|'
-                  r'pour|fin[ _]pour|à|déclarations?|juqsque|spécialise|specialise|comporte|super|public|privé|protégé|'
-                  r'classe'
+        'core': [  # Keywords
+                 (r'\b(read|obtain|get'              # Input
+                  r'print|display|show'              # Output
+                  r'compute|calculate|determine'     # Compute
+                  r'set|initialize|init|let'         # Initialize
+                  r'increment|bump|decrement'        # Add/Sub one
+                  r'if[ _]then|if|then|else|endif'   # If-Then-Else
+                  r'while|do|endwhile|done'          # (Do) While
+                  r'case|of|others|endcase'          # Case
+                  r'repeat[ _]until|repeat|until'    # Repeat Until
+                  r'for|endfor'                      # For
+                  r'call|exception|when|as|recurse'  # Program Flow
                   r')\s*\b', Keyword),
 
                  # Data Types
-                 (r'\b(entiers?|chaines?|chaînes?|réels?|reels?|caractères?|caracteres?|booléens?|'
-                  r'booleens?|tableaux?|rien)\s*\b', 
+                 (r'\b(integer?|string?|float?|character?'
+                  r'boolean?|array?|nothing)\s*\b',
                   Keyword.Type),
 
-                  (r'\b(vrai|faux|nil)\s*\b',
-                   Name.Constant),
-                  
+                 (r'\b(true|false|nil|null)\s*\b',
+                  Name.Constant),
+
                  # Operators
-                 (r'(<=|>=|<>|<-|\^|\*|\+|-|\/|<|>|=|\\\\|mod|←|↑|≤|≥|≠|÷|×|\.\.|\[|\]|\.|non|xou|et|ou)',
+                 (r'(<=|>=|<>|<-|\^|\*|\+|-|\/|<|>|=|\\\\|%'
+                  r'←|↑|≤|≥|≠|÷|×|\.\.|\[|\]|\.|not|xor|and|or)',
                   op_replace),
-                  
+
                  (r'(\(|\)|\,|\;|:)',
                   Punctuation),
-                  
-                 #(r'\b(\[(VE|VS|VE/S)\])\s*\b',
-                 # Keyword.Declaration),
 
-                  # Intrinsics
-                 (r'\b(sqrt|pow|cos|sin|tan|arccos|arcsin|arctan|arctan2|lire|ecrire|écrire|'
-                  r'exp|ln|log|détruire|detruire'
+                 # Intrinsics
+                 (r'\b(sqrt|pow|cos|sin|tan|arccos|arcsin|arctan'
+                  r'arctan2|add|subtract|multiply|divide'
+                  r'exp|ln|log'
                   r')\s*\b', Name.Builtin)
                 ],
 
@@ -90,4 +89,3 @@ class PseudocodeLexer(RegexLexer):
                  (r'[+-]?\d+\.\d*([eE][-+]?\d+)?', Number.Float)
                 ],
         }
-        
