8e2d06fa424779dfc210609d4305967db9f644c6
Christopher Rodriguez <yewscion@gmail.com>
2022-08-12 14:37:10 -0400

    Fixed Author and License.

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ setup(
     packages=["pygments_lexer_pseudotaxus"],
     version="2.0.1",
     description="Pygments Lexer for Pseudotaxus Pseudocode",
-    author="Simon Wachter",
+    author="Christopher Rodriguez",
     author_email="yewscion@gmail.com",
     url="https://git.sr.ht/~yewscion/pygments-lexer-pseudocode-std",
-    license="AGPL3",
     entry_points="[pygments.lexers]\npseudotaxuslexer = pygments_lexer_pseudocode:PseudotaxusLexer",
     install_requires=[ "pygments" ],
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Topic :: Text Processing",
         "Topic :: Utilities",
     ]
